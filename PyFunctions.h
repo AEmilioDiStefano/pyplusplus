3bfbe21e9394c631608148e5837760b2f6524d96
AEmilioDiStefano <160543828+AEmilioDiStefano@users.noreply.github.com>
2025-05-27 00:38:31 -0400

    Create PyFunctions.h

---
+++ PyFunctions.h
@@ -0,0 +1,10 @@
+#ifndef REMEMBERER_H
+#define REMEMBERER_H
+
+#include <iostream>
+
+class PyFunctions {
+  public:
+    std::vector<std::string> PyFunctions::readlines(std::string& file_name);
+  private:
+}
